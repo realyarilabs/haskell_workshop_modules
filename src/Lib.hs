51efd9c1c364375f54c00c2ded5cd2f752097bcb
Xavier Francisco <xavier.n.francisco@gmail.com>
2019-01-17 22:43:48 +0000

    step2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,9 @@
 module Lib (someFunc) where
 
-import qualified Greet.English
-import qualified Greet.Portuguese
-import qualified Greet.French
-
 import InteractLine (interactLine)
 
 someFunc :: IO ()
 someFunc = interactLine greet
 
 greet :: String -> String
-greet "en" = Greet.English.greet 
-greet "fr" = Greet.French.greet
-greet "pt" = Greet.Portuguese.greet
+greet _ = undefined
