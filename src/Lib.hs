c37ae0bf8e6096bc65de0174f0f442eb7a422bf3
Xavier Francisco <xavier.n.francisco@gmail.com>
2019-01-17 20:28:00 +0000

    doesn't compile anymore :shrug:

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,14 @@
 module Lib (someFunc) where
 
+import Greet.English
+import Greet.Portuguese
+import Greet.World
 import InteractLine
 
 someFunc :: IO ()
 someFunc = interactLine greet
 
 greet :: String -> String
-greet = undefined
+greet "pt" = undefined
+greet "en" = undefined
+greet _ = undefined
